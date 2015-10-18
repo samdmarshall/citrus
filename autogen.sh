705e7ea9fb8efaa72d6cb912236447a7a43e283d
Gwynne Raskind <gwynne@darkrainfall.org>
2015-10-18 04:36:45 -0500

    Switch to Automake, meh. Also includes an autoupdate run on configure.ac and switching to the generic "DEBUG" flag for debug builds.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-LIBTOOLIZE=${LIBTOOLIZE:-$( which libtoolize )}
-if [ -z $LIBTOOLIZE ]; then
-	LIBTOOLIZE=$( which glibtoolize )
-fi
 AUTORECONF=${AUTORECONF:-$( which autoreconf )}
 
-${LIBTOOLIZE} --copy --install
-${AUTORECONF}
+${AUTORECONF} --install -Wall --verbose --force
 
